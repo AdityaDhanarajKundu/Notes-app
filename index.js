123ba5c44966fd3a59aa2b9c4e659eb858ca661d
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-08-17 10:56:55 +0530

    added the delete notes and the update localStorage functions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+//function to create notes
 createBtn.addEventListener("click",()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -9,4 +10,27 @@ createBtn.addEventListener("click",()=>{
     inputBox.setAttribute("contenteditable","true");
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
-})
+});
+
+//function to delete the notes
+notesContainer.addEventListener("click",(e)=>{
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes.forEach(element=>{
+            element.addEventListener("onkeyup",updateStorage);
+        });
+    }
+});
+
+//function to save the notes in the localStorage
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+
+//function to display the saved notes
+function showNotes(){
+    
+}
